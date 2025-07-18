771ebdbf9ccceff812d85421322aae5df0de24a7
Rodrigo Rizzo <38816338+rodrigorizzo@users.noreply.github.com>
2025-07-17 22:57:41 -0300

    Implementei legenda com nome do autor no projeto slide-image

--- slide-imagens/src/App.jsx
+++ slide-imagens/src/App.jsx
@@ -1,18 +1,37 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-  let [id, setId] = useState(28);
+  let [id, setId] = useState(0);
+  let [info, setInfo] = useState({});
+
+  const fetchInfo = useEffect(() => {
+    fetch(`https://picsum.photos/id/${id}/info`)
+      .then((res) => res.json())
+      .then((res) => {
+        console.log(res);
+        setInfo(res);
+      });
+  }, [id]);
 
   const trocarImagem = (num) => {
     setId(id + num);
+    fetchInfo;
   };
 
   return (
     <div>
-      <img src={`https://picsum.photos/id/${id}/300`}></img>
-      <button onClick={() => trocarImagem(-1)}>Anterior</button>
-      <button onClick={() => trocarImagem(1)}>Próximo</button>
+      <div>
+        <button onClick={() => trocarImagem(-1)}> &#65308; </button>
+        <figure>
+          <img src={`https://picsum.photos/id/${id}/300`}></img>
+          <figcaption>{info.author}</figcaption>
+        </figure>
+        <button onClick={() => trocarImagem(1)}> &#xFF1E; </button>
+      </div>
+      <div>
+        <button>Print</button>
+      </div>
     </div>
   );
 }
