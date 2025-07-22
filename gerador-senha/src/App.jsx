33ad839a64534e48ba92f342fee11958e82ca6ab
Rodrigo Rizzo <38816338+rodrigorizzo@users.noreply.github.com>
2025-07-22 20:49:29 -0300

    Gerador de senha: Implementei a função gerarSenha e adicionei as variáveis necessárias para as configurações

--- gerador-senha/src/App.jsx
+++ gerador-senha/src/App.jsx
@@ -1,7 +1,26 @@
 import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [senha, setSenha] = useState(0);
+  const [quant, setQuant] = useState(10);
+  const [numeros, setNumeros] = useState(false);
+  const [caracteres, setCaracteres] = useState(false);
+
+  const gerarSenha = () => {
+    let novaSenha = "";
+
+    const stringCaracteres =
+      "AaBbCcDdEeFfGgHhIiJjLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz";
+    const num = "0123456789";
+    const especiais = "@#$%&*!";
+
+    for (let i = 0; i < quant; i++) {
+      const index = Math.floor(Math.random() * stringCaracteres.length);
+      novaSenha += stringCaracteres.charAt(index);
+    }
+
+    setSenha(novaSenha);
+  };
 
   return (
     <div className="flex flex-col gap-3 w-fit bg-gray-200 p-5 m-auto">
@@ -13,15 +32,23 @@ function App() {
           name=""
           id=""
           placeholder="senha-aleatoria"
+          value={senha}
         />
-        <button className="bg-blue-900 text-white inline-block p-2">copiar</button>
+        <button
+          onClick={gerarSenha}
+          className="bg-blue-900 text-white inline-block p-2"
+        >
+          copiar
+        </button>
       </div>
       <div className="flex flex-wrap gap-1">
         <label className="whitespace-nowrap" htmlFor="">
-          <input  type="range" name="" id="" /> 
+          <input type="range" name="" id="" min={4} max={16} />
+          {quant}
         </label>
         <label className="whitespace-nowrap">
-          <input className="whitespace-nowrap"type="checkbox" name="" id="" /> Números
+          <input className="whitespace-nowrap" type="checkbox" name="" id="" />{" "}
+          Números
         </label>
         <label className="whitespace-nowrap">
           <input type="checkbox" name="" id="" /> Caracteres especiais
